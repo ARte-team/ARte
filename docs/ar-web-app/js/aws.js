43daf1c371232a8064d9851322af52bed891ee13
Claudiu Ivan <claudiu97@hotmail.com>
2020-05-25 16:49:58 +0200

    AR web app (2)

---
+++ docs/ar-web-app/js/aws.js
@@ -0,0 +1,54 @@
+"use strict";
+
+/**
+* utilities to do sigv4
+* @class SigV4Utils
+*/
+function SigV4Utils() {}
+
+SigV4Utils.getSignatureKey = function(key, date, region, service) {
+  var kDate = AWS.util.crypto.hmac('AWS4' + key, date, 'buffer');
+  var kRegion = AWS.util.crypto.hmac(kDate, region, 'buffer');
+  var kService = AWS.util.crypto.hmac(kRegion, service, 'buffer');
+  var kCredentials = AWS.util.crypto.hmac(kService, 'aws4_request', 'buffer');
+  return kCredentials;
+};
+
+SigV4Utils.getSignedUrl = function(host, region, credentials) {
+  // Create a canonical request for Signature Version 4
+  var datetime = AWS.util.date.iso8601(new Date()).replace(/[:\-]|\.\d{3}/g, '');
+  var date = datetime.substr(0, 8);
+
+  var method = 'GET';
+  var protocol = 'wss';
+  var uri = '/mqtt';
+  var service = 'iotdevicegateway';
+  var algorithm = 'AWS4-HMAC-SHA256';
+
+  var credentialScope = date + '/' + region + '/' + service + '/' + 'aws4_request';
+  var canonicalQuerystring = 'X-Amz-Algorithm=' + algorithm;
+  canonicalQuerystring += '&X-Amz-Credential=' + encodeURIComponent(credentials.accessKeyId + '/' + credentialScope);
+  canonicalQuerystring += '&X-Amz-Date=' + datetime;
+  canonicalQuerystring += '&X-Amz-SignedHeaders=host';
+
+  var canonicalHeaders = 'host:' + host + '\n';
+  var payloadHash = AWS.util.crypto.sha256('', 'hex');
+  var canonicalRequest = method + '\n' + uri + '\n' + canonicalQuerystring + '\n' + canonicalHeaders + '\nhost\n' + payloadHash;
+
+  // Create a string to sign, generate a signing key, and sign the string
+  var stringToSign = algorithm + '\n' + datetime + '\n' + credentialScope + '\n' + AWS.util.crypto.sha256(canonicalRequest, 'hex');
+  var signingKey = SigV4Utils.getSignatureKey(credentials.secretAccessKey, date, region, service);
+  var signature = AWS.util.crypto.hmac(signingKey, stringToSign, 'hex');
+
+  // Add the signing information to the request
+  canonicalQuerystring += '&X-Amz-Signature=' + signature;
+
+  // If you have session credentials (from an STS server, AssumeRole, or Amazon Cognito), append the session token to the end of the URL string after signing
+  if (credentials.sessionToken) {
+    canonicalQuerystring += '&X-Amz-Security-Token=' + encodeURIComponent(credentials.sessionToken);
+  }
+
+  // Prepend the protocol, host, and URI to the canonicalQuerystring
+  var requestUrl = protocol + '://' + host + uri + '?' + canonicalQuerystring;
+  return requestUrl;
+};
---
+++ docs/ar-web-app/js/pahoMQTTClient.js
@@ -0,0 +1,87 @@
+"use strict";
+
+class PahoMQTTClient {
+
+  constructor(requestUrl, clientId) {
+    this.requestUrl  = requestUrl;
+    this.clientId    = clientId;
+    this.client      = null;
+    this.isConnected = false;
+  }
+
+  // Connect to the server
+  conn(callbackConnection, callbackReceive) {
+    this.client = new Paho.MQTT.Client(this.requestUrl, this.clientId);
+
+    var connectOptions = {
+      onSuccess: () => {
+        // Connect succeeded
+        console.log("onConnect: connect succeeded");
+        // With an arrow function "this" represents the owner of the function
+        // while with a regular function "this" represents the object that calls the function
+        this.isConnected = true;
+        callbackConnection();
+      },
+      useSSL: true,
+      timeout: 3,
+      mqttVersion: 4,
+      onFailure: function() {
+        // Connect failed
+        console.log("onFailure: connect failed");
+        callbackConnection();
+      }
+    };
+
+    // Set callback handlers
+    this.client.onConnectionLost = onConnectionLost;
+    this.client.onMessageArrived = onMessageArrived;
+
+    // Connect the client
+    this.client.connect(connectOptions);
+
+
+    // Called when the client loses its connection
+    function onConnectionLost(responseObject) {
+      if (responseObject.errorCode !== 0)
+        console.log("onConnectionLost:" + responseObject.errorMessage);
+    }
+
+    // Called when a message arrives
+    function onMessageArrived(message) {
+      console.log("onMessageArrived:" + message.payloadString);
+      callbackReceive(message.payloadString);
+    }
+
+  }
+
+  // Subscribe to a topic
+  sub(topic) {
+    console.log("Subscribing on topic " + topic);
+    this.client.subscribe(topic);
+  }
+
+  // Publish a message on a topic
+  pub(message, topic) {
+    console.log("Publishing message on topic " + topic);
+    // QOS = 0 => Best effort, retained = false => Message delivered only to current subscriptions
+    this.client.send(topic, message, 0, false);
+  }
+
+  // Unsubscribe to a topic
+  unsub(topic) {
+    console.log("Unsubscribing on topic " + topic);
+    this.client.unsubscribe(topic);
+  }
+
+  // Disconnect the client
+  disc() {
+    console.log("Disconnecting the client");
+    this.client.disconnect();
+    this.isConnected = false;
+  }
+
+  isConn() {
+    return this.isConnected;
+  }
+
+}
