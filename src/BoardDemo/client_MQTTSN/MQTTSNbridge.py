bf56ab0f146734a5c7609017be5a7de839532f86
Francesco31Ott <francy.3131@gmail.com>
2020-05-25 10:17:48 +0200

    Added Board Demo

---
+++ src/BoardDemo/client_MQTTSN/MQTTSNbridge.py
@@ -0,0 +1,58 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import MQTTSNclient
+import json
+import boto3
+
+# connection to DynamoDB and access to the table EnvironmentalStationDB
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+dynamoTable = dynamodb.Table('RoomsDB')
+jsonP = ''
+
+# clients for MQTT and MQTTS
+MQTTClient = AWSIoTMQTTClient("MQTTSNbridge")
+MQTTSNClient = MQTTSNclient.Client("bridge", port=1885)
+
+class Callback:
+  # function that replies a message from the MQTTSN broker to the MQTT one
+  # and inserts into the database the message just arrived
+  def messageArrived(self, topicName, payload, qos, retained, msgid):
+      message = payload.decode("utf-8")
+      jsonP = json.loads(message) 
+      print(topicName, message)
+      MQTTClient.publish(topicName, message, qos)
+      dynamoTable.put_item(Item=jsonP)
+      return True
+
+# path that indicates the certificates position
+path = "../certs/"
+
+# configure the access with the AWS MQTT broker
+MQTTClient.configureEndpoint("a3um1mnv6jt2hg-ats.iot.us-east-1.amazonaws.com", 8883)
+MQTTClient.configureCredentials(path+"root-CA.crt",
+                                path+"private.pem.key",
+                                path+"certificate.pem.crt")
+
+# configure the MQTT broker
+MQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+MQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+MQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+MQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# register the callback
+MQTTSNClient.registerCallback(Callback())
+
+# make connections to the clients
+MQTTClient.connect()
+MQTTSNClient.connect()
+
+# subscribe to the topic
+MQTTSNClient.subscribe("sensor/room")
+
+# cycle that wait for a command to close the program
+while True:
+    if input("Enter 'quit' to exit from the program.\n") == "quit":
+        break
+
+# disconnect from the clients
+MQTTSNClient.disconnect()
+MQTTClient.disconnect()
---
+++ src/BoardDemo/emcute_MQTTSN/bin/native/tinymt32/tinymt32.d
@@ -0,0 +1,34 @@
+/home/francesco/Documenti/UNIVERSITÀ/Magistrale/1Anno2Semestre/InternetOfThings/RIOT/examples/emcute_mqttsn/bin/native/tinymt32/tinymt32.o: \
+ /home/francesco/Documenti/UNIVERSITÀ/Magistrale/1Anno2Semestre/InternetOfThings/RIOT/sys/random/tinymt32/tinymt32.c \
+ /usr/include/stdc-predef.h \
+ /home/francesco/Documenti/UNIVERSITÀ/Magistrale/1Anno2Semestre/InternetOfThings/RIOT/examples/emcute_mqttsn/bin/native/riotbuild/riotbuild.h \
+ /home/francesco/Documenti/UNIVERSITÀ/Magistrale/1Anno2Semestre/InternetOfThings/RIOT/sys/random/tinymt32/tinymt32.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h /usr/include/stdint.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-32.h /usr/include/bits/wchar.h \
+ /usr/include/inttypes.h
+
+/usr/include/stdc-predef.h:
+
+/home/francesco/Documenti/UNIVERSITÀ/Magistrale/1Anno2Semestre/InternetOfThings/RIOT/examples/emcute_mqttsn/bin/native/riotbuild/riotbuild.h:
+
+/home/francesco/Documenti/UNIVERSITÀ/Magistrale/1Anno2Semestre/InternetOfThings/RIOT/sys/random/tinymt32/tinymt32.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h:
+
+/usr/include/stdint.h:
+
+/usr/include/features.h:
+
+/usr/include/sys/cdefs.h:
+
+/usr/include/bits/wordsize.h:
+
+/usr/include/gnu/stubs.h:
+
+/usr/include/gnu/stubs-32.h:
+
+/usr/include/bits/wchar.h:
+
+/usr/include/inttypes.h:
