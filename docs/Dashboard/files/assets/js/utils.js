1ff3c137b039b54dac790773f017af7aa2d2844f
Ivan <xmetal1997@gmail.com>
2020-12-13 20:56:06 +0100

    Updated backend, Added today museum flow in General page and dynamic availability card, Connected artworks most viewed table with the backend, Added artwork availability in Add and Edit pages, Added login with the backend & lil fixes

--- docs/Dashboard/files/assets/js/utils.js
+++ docs/Dashboard/files/assets/js/utils.js
@@ -163,19 +163,6 @@ function animateValue(elem, start, end, duration, step=10) {
   }, stepTime);
 }
 
-// Convert to 32bit integer
-/*function stringToHash(string) {
-  let hash = 0;
-
-  for (i = 0; i < string.length; i++) {
-    char = string.charCodeAt(i);
-    hash = ((hash << 5) - hash) + char;
-    hash = hash & hash;
-  }
-
-  return hash;
-}*/
-
 // TODO
 async function resizeImage(document, file) {
   var img = document.createElement("img");
